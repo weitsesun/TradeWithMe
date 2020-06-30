c133a67cdffa8fd56207d78ee7e1e736e237be73
Wayne Sun <wtse.sun@gmail.com>
2020-06-30 11:44:11 -0700

    renamed folder for better structure view

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, SafeAreaView, View, Dimensions } from "react-native";
-import WelcomePage from "./components/screens/WelcomePage";
-import ViewImageScreen from "./components/screens/ViewImageScreen";
+import WelcomePage from "./app/screens/WelcomePage";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  // return <WelcomePage />;
-  return <ViewImageScreen />;
+  return <WelcomePage />;
+  // return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
