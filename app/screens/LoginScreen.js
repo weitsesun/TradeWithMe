250e5af3ce3f19ad05ac689216a67d2967e8519e
Wayne Sun <wtse.sun@gmail.com>
2020-07-08 19:29:23 -0700

    created login screen with email and password section

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,18 +1,50 @@
-import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Image } from "react-native";
 
 import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
 
 function LoginScreen(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
-    <Screen>
-      <Image source={require("../assets/logo.png")} />
+    <Screen style={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+      <AppTextInput
+        placeholder="Email"
+        icon="email"
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        onChangeText={(text) => setEmail(text)}
+      />
+      <AppTextInput
+        placeholder="Password"
+        icon="lock"
+        autoCapitalize="none"
+        autoCorrect={false}
+        textContentType="password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <AppButton title="Login" onPress={() => console.log(email, password)} />
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 60,
+  },
 });
 
 export default LoginScreen;
