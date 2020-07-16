07ce6a34ade8464a0e0a450e2b3b1b86e61366ec
Wayne Sun <wtse.sun@gmail.com>
2020-07-16 14:04:08 -0700

    finished formImagePicker

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageInputList from "../ImageInputList";
+import { ErrorMessage } from ".";
+
+function FormImagePicker(props) {
+  return;
+  <>
+    <ImageInputList />
+    <ErrorMessage />
+  </>;
+}
+
+export default FormImagePicker;
