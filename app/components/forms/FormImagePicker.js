4a263f0d785500f2c34e88305b0a5fe3a6d4e658
Wayne Sun <wtse.sun@gmail.com>
2020-07-16 14:08:49 -0700

    created a form component as image picker

--- app/components/forms/FormImagePicker.js
+++ app/components/forms/FormImagePicker.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useFormikContext } from "formik";
+
+import ErrorMessage from "./ErrorMessage";
 import ImageInputList from "../ImageInputList";
-import { ErrorMessage } from ".";
 
 function FormImagePicker(props) {
-  return;
-  <>
-    <ImageInputList />
-    <ErrorMessage />
-  </>;
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setImageUris((prev) => prev.filter((imageUri) => imageUri !== uri));
+  };
+
+  return (
+    <>
+      <ImageInputList
+        imageUris={values[name]}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage />
+    </>
+  );
 }
 
 export default FormImagePicker;
