b063cfc918339639e1bbe332d30d61962904ad63
Wayne Sun <wtse.sun@gmail.com>
2020-08-05 14:09:07 -0700

    built cashe layer with get & isExpire function

--- app/utility/cashe.js
+++ app/utility/cashe.js
@@ -1,6 +1,8 @@
 import AsyncStorage from "@react-native-community/async-storage";
+import moment from "moment";
 
 const prefix = "cache";
+const expiryInMinutes = 5;
 
 const store = async (key, value) => {
   try {
@@ -14,10 +16,28 @@ const store = async (key, value) => {
   }
 };
 
+const isExpired = (item) => {
+  let now = moment(Date.now());
+  let storeTime = moment(item.timestamp);
+
+  return now.diff(storeTime, "minutes") > expiryInMinutes;
+};
+
 const get = async (key) => {
   try {
     const value = await AsyncStorage.getItem(prefix + key);
     const item = JSON.parse(value);
+
+    if (!item) return null;
+
+    if (isExpired(item)) {
+      // Invalid Command Query Separation (CQS) principle here, but it's ok
+      // Where invalid? A GET query should not change anything in the database
+      await AsyncStorage.removeItem(prefix + key);
+      return null;
+    }
+
+    return item.value;
   } catch (error) {
     console.log(error);
   }
