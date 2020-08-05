35aeb992cec741f3bce23569c8fd3109aac1cb47
Wayne Sun <wtse.sun@gmail.com>
2020-08-05 14:23:18 -0700

    added get function to local storage when no wifi

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,23 @@
 import { create } from "apisauce";
+import cache from "../utility/cashe";
 
 const apiClient = create({
   baseURL: "http://192.168.1.4:9000/api",
 });
+
+const get = apiClient.get;
+apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  // We are at this point when failed to connect to server
+  // Try get data from local storage
+  const data = await cache.get(url);
+
+  return data ? { ok: true, data } : response;
+};
 
 export default apiClient;
