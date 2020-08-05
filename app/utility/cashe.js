964a85d1a413aeaf8fc3ec8dc2553bed0c709f0f
Wayne Sun <wtse.sun@gmail.com>
2020-08-05 13:54:56 -0700

    created store and get function for local storage

---
+++ app/utility/cashe.js
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-community/async-storage";
+
+const prefix = "cache";
+
+const store = async (key, value) => {
+  try {
+    const item = {
+      value,
+      timestamp: Date.now(),
+    };
+    await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const get = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(prefix + key);
+    const item = JSON.parse(value);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default {
+  store,
+};
