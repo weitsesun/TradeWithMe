9761c16edf81cd70e72fb755c67f940ffb58826b
Wayne Sun <wtse.sun@gmail.com>
2020-08-05 19:48:21 -0700

    removed console.log

--- app/components/OfflineNotice.js
+++ app/components/OfflineNotice.js
@@ -7,7 +7,6 @@ import { useNetInfo } from "@react-native-community/netinfo";
 
 function OfflineNotice() {
   const netInfo = useNetInfo();
-  console.log(netInfo);
   if (netInfo.type !== "unknown" && netInfo.isInternetReachable === false) {
     return (
       <View style={styles.container}>
